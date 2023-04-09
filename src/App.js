5d3ad70cb8e59b0aaf15ad3eab8e4850c1dadc18
matt <mathewandnana@gmail.com>
2023-04-08 21:24:31 -0500

    editing first game making restraints on target spawn in and div size

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 
 function Target(props) {
@@ -50,8 +24,21 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       const newTargets = [...targets];
-      const x = Math.floor(Math.random() * (window.innerWidth - 50));
-      const y = Math.floor(Math.random() * (window.innerHeight - 50));
+      let x, y, overlap;
+      do {
+        overlap = false;
+        x = Math.floor(Math.random() * (900 - 50));
+        y = Math.floor(Math.random() * (500 - 50));
+        for (let i = 0; i < newTargets.length; i++) {
+          const distance = Math.sqrt(
+            (x - newTargets[i].x) ** 2 + (y - newTargets[i].y) ** 2
+          );
+          if (distance < 100) {
+            overlap = true;
+            break;
+          }
+        }
+      } while (overlap);
       newTargets.push({ x, y });
       setTargets(newTargets);
     }, 1000);
@@ -66,15 +53,25 @@ function App() {
   }
 
   return (
-    <div>
-      {targets.map((target, index) => (
-        <Target
-          key={index}
-          x={target.x}
-          y={target.y}
-          onClick={() => handleTargetClick(index)}
-        />
-      ))}
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        backgroundColor: 'black'
+      }}
+    >
+      <div style={{ width: '900px', height: '500px', position: 'relative', backgroundColor: 'white' }}>
+        {targets.map((target, index) => (
+          <Target
+            key={index}
+            x={target.x}
+            y={target.y}
+            onClick={() => handleTargetClick(index)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
